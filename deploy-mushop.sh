dc8a83b646410c8b50e2b55c7f6bf3ea1d98af71
Prasenjit Sarkar <jit2600@gmai.com>
2020-08-27 19:54:12 +0100

    deploy mushop shell script added

---
+++ deploy-mushop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+kubectl create namespace mushop
+kubectl create namespace mushop-utilities
+kubectl config set-context --current --namespace=mushop
+helm dependency update deploy/complete/helm-chart/setup
+helm install mushop-utils deploy/complete/helm-chart/setup --namespace mushop-utilities
+helm install mushop deploy/complete/helm-chart/mushop -f deploy/complete/helm-chart/mushop/values-mock.yaml
+kubectl get svc -n mushop-utilities | grep ingress-controller
