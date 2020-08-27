658a07b2951ad32dd315bf320b8d84e5b2aa2421
stretchcloud <jit2600@gmail.com>
2020-08-27 20:08:20 +0100

    Adding sleep

--- deploy-mushop.sh
+++ deploy-mushop.sh
@@ -4,5 +4,6 @@ kubectl create namespace mushop-utilities
 kubectl config set-context --current --namespace=mushop
 helm dependency update deploy/complete/helm-chart/setup
 helm install mushop-utils deploy/complete/helm-chart/setup --namespace mushop-utilities
+sleep 10
 helm install mushop deploy/complete/helm-chart/mushop -f deploy/complete/helm-chart/mushop/values-mock.yaml
 kubectl get svc -n mushop-utilities | grep ingress-controller
